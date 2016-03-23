76266a1d2158a0bb68fa4a69dbbfb6daf7a275d6
Alfred Bratterud <alfred.bratterud@hioa.no>
2016-03-23 13:23:22 +0100

    Checking for updates at login

--- emacs_env.sh
+++ emacs_env.sh
@@ -1,17 +1,8 @@
 # Dot-source this in e.g. .bashrc
 
-# Environment vars 
-export INCLUDEOS_SRC="~/IncludeOS"
-export OS="$INCLUDEOS_SRC/src"
-export TEST="~/IncludeOS/test"
-export APP="$TEST/transmit"
-
-# Aliases
-alias os="cd $OS"
-alias app="cd $APP"
-
-# -nw is useful when you work in an environment with emacs-GUI (I don't use it)
-alias emacs="emacs -nw"
-
-# Most common typo
-alias eamcs="emacs"
+# Get updates
+echo -e "\n>>> Getting emacs environment updates"
+pushd ~/.emacs.d/
+git pull origin master
+. ./env_vars.sh
+popd
