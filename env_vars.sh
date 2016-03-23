bc4112d26b4dd682c163a604395bd32876dbcdd1
Alfred Bratterud <alfred.bratterud@hioa.no>
2016-03-23 13:22:13 +0100

    Checking for updates at login

---
+++ env_vars.sh
@@ -0,0 +1,15 @@
+# Environment vars 
+export INCLUDEOS_SRC="~/IncludeOS"
+export OS="$INCLUDEOS_SRC/src"
+export TEST="~/IncludeOS/test"
+export APP="$TEST/transmit"
+
+# Aliases
+alias os="cd $OS"
+alias app="cd $APP"
+
+# -nw is useful when you work in an environment with emacs-GUI (I don't use it)
+alias emacs="emacs -nw"
+
+# Most common typo
+alias eamcs="emacs"
