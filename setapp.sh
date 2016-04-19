3f750a0218621f2729539f3b2b66b806dbe0948b
Alfred Bratterud <alfred.bratterud@hioa.no>
2016-04-19 08:34:36 +0200

    setapp with no params now fetches app name from disk

--- setapp.sh
+++ setapp.sh
@@ -1,14 +1,22 @@
 #! /bin/bash
 
-[ -d $INCLUDEOS_SRC/test/$1 ] || { echo "Couldn't find test-directory $1" && return 1; }
+if [ "$#" -lt 1 ] 
+then
+    echo "Fetching app from disk"
+    export APP=`cat ~/.emacs.d/app`    
+else
+    [ -d $INCLUDEOS_SRC/test/$1 ] || { echo "Couldn't find test-directory $1" && return 1; }
+    echo "Switching app to $INCLUDEOS_SRC/test/$1/"
+    pushd ~/.emacs.d
+    export APP=$TEST/$1
+    echo $1 > ./app
+    git add ./app
+    git commit -m "Switching app to ./test/$1"
+    git push origin master    
+    popd
+fi
 
-echo "Switching app to $INCLUDEOS_SRC/test/$1/"
-pushd ~/.emacs.d
-export APP=$TEST/$1
-echo $1 > ./app
-git add ./app
-git commit -m "Switching app to ./test/$1"
-git push origin master
-
+echo "App is now $APP"
 alias app="cd $APP"
-popd
+
+
